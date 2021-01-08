7d47a318ad3bd873e96885c15ea72f17471829a9
忧里修斯 <769435570@qq.com>
2021-01-08 22:33:55 +0800

    相机脚本

---
+++ Assets/Scripts/CameraTest/CameraCtrl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace CameraTest
+{
+    public class CameraCtrl : MonoBehaviour
+    {
+        private GameObject _cameraRig;
+        private Transform _cameraTransform;
+        private GameObject _player;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _cameraRig = GameObject.Find("CameraRig");
+            _cameraTransform = _cameraRig.transform;
+
+            _player = GameObject.Find("Ellen");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 targetPos = _cameraTransform.position + new Vector3(0, 1, 0);
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, targetPos, Time.deltaTime);
+            _cameraTransform.LookAt(_player.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/InputTest/InputCtrl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace InputTest
+{
+    public class InputCtrl : MonoBehaviour
+    {
+       
+
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+    
+}
