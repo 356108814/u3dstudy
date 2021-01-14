92f9d36b09655bad2c90ff4e0cc791ddec2f52b4
忧里修斯 <769435570@qq.com>
2021-01-14 22:25:03 +0800

    增加摄像机跟随和移动

--- Assets/Scripts/CameraTest/CameraCtrl.cs
+++ Assets/Scripts/CameraTest/CameraCtrl.cs
@@ -11,18 +11,18 @@ namespace CameraTest
         // Start is called before the first frame update
         void Start()
         {
-            _cameraRig = GameObject.Find("CameraRig");
-            _cameraTransform = _cameraRig.transform;
-
-            _player = GameObject.Find("Ellen");
+            // _cameraRig = GameObject.Find("CameraRig");
+            // _cameraTransform = _cameraRig.transform;
+            //
+            // _player = GameObject.Find("Ellen");
         }
 
         // Update is called once per frame
         void Update()
         {
-            Vector3 targetPos = _cameraTransform.position + new Vector3(0, 1, 0);
-            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, targetPos, Time.deltaTime);
-            _cameraTransform.LookAt(_player.transform.position);
+            // Vector3 targetPos = _cameraTransform.position + new Vector3(0, 1, 0);
+            // _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, targetPos, Time.deltaTime);
+            // _cameraTransform.LookAt(_player.transform.position);
         }
     }
 }
---
+++ Assets/Scripts/CameraTest/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace CameraTest
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        private float _disY = 4;
+        private float _disZ = -8;
+        private Transform _followTarget;
+        private Vector3 _offset;
+
+        private Vector3 _targetPos;
+        private float _damping = 1; //摄像机旋转阻尼 
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _followTarget = GameObject.FindWithTag("Player").transform;
+            _offset = new Vector3(0, 4, -8);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Quaternion rotation = Quaternion.Euler(0, _followTarget.eulerAngles.y, 0); //将玩家Y轴旋转转换为四元数（代表一个三维旋转）  
+            //rotation * offset：四元数*一个向量代表=将该向量转四元数代表的角度，即将该旋转应用到该向量，得到一个新的向量。体现为摄像机随着角色的旋转而左右旋转（但还没以角色为中心注视）  
+            _targetPos = _followTarget.position + (rotation * _offset);
+            transform.position = Vector3.Lerp(transform.position, _targetPos, Time.deltaTime * _damping);
+            transform.LookAt(_followTarget);
+        }
+    }
+}
